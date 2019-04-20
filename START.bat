a2690c1076bb66f512ee01122e222ac0ffc327cd
Jamie <nanobugxl@gmail.com>
2019-04-20 01:08:15 -0400

    Update START.bat

--- START.bat
+++ START.bat
@@ -8,7 +8,7 @@ Title ...... Loading
 echo "Hey, thanks for using WingateXL CLI" && pause
 echo The Time Now is %DATE%
 Title Hang on a moment.....
-timeout 5 && echo Be sure to visit our Pro Builders Server at 158.69.33.159-25567 & timeout 3 
+timeout 5
 echo Enjoy your game Builder!
 java -jar launcher.jar --server [THESERVERYOUWANTTOCONNECTTOHERE] --port 12345 -Xms1G -Xmx3G -d64 -jar minecraft_server.1.13.2.jar nogui
 
